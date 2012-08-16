c98b45ffd518b807d9c94ccb88bf62e234abc6e4
Jason Spafford <nullprogrammer@gmail.com>
2012-08-16 00:40:28 -0700

    Begun adding patch to show form installation

---
+++ src/PluginUpdater/frmPatch.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SpaceportUpdaterPlugin
+{
+	partial class frmPatch
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose ();
+			}
+			base.Dispose (disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.inNotes = new System.Windows.Forms.TextBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.btnInstall = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// inNotes
+			// 
+			this.inNotes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.inNotes.Location = new System.Drawing.Point(14, 36);
+			this.inNotes.Multiline = true;
+			this.inNotes.Name = "inNotes";
+			this.inNotes.ReadOnly = true;
+			this.inNotes.Size = new System.Drawing.Size(451, 207);
+			this.inNotes.TabIndex = 0;
+			this.inNotes.Text = "Fixes:\r\n\t- Fixed some bugs in A2J\r\n\t- Fixed a crash in the simulator\r\nEnhancement" +
+    "s:\r\n\t- Added new feature\r\n";
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.progressBar1.Location = new System.Drawing.Point(14, 257);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(352, 27);
+			this.progressBar1.TabIndex = 1;
+			// 
+			// btnInstall
+			// 
+			this.btnInstall.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnInstall.Location = new System.Drawing.Point(378, 257);
+			this.btnInstall.Name = "btnInstall";
+			this.btnInstall.Size = new System.Drawing.Size(87, 27);
+			this.btnInstall.TabIndex = 2;
+			this.btnInstall.Text = "Install";
+			this.btnInstall.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 8);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(240, 21);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Preparing to install patch v0.0.2.0";
+			// 
+			// frmPatch
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(477, 296);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnInstall);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.inNotes);
+			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "frmPatch";
+			this.Text = "Patch";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox inNotes;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Button btnInstall;
+		private System.Windows.Forms.Label label1;
+
+	}
+}
