2213ac5aff4655b98a9581058bf61c3279c52757
Jason Spafford <nullprogrammer@gmail.com>
2013-03-29 16:59:47 -0700

    Added a string helper with split (string)

---
+++ src/Launchpad/Helpers/StringHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Launchpad.Helpers
+{
+	public static class StringHelper
+	{
+		public static string[] Split (this string self, string split)
+		{
+			return self.Split (new[] { split },
+				StringSplitOptions.RemoveEmptyEntries);
+		}
+	}
+}
