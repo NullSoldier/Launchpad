df36f25fad34207f999eba68bf826ef0056895ed
Jason Spafford <nullprogrammer@gmail.com>
2013-03-29 17:16:09 -0700

    Added base for config data binding

---
+++ src/Launchpad/Config/DeviceInfo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UpdaterCore;
+
+namespace Launchpad.Config
+{
+	public enum Orientation
+	{
+		Portrait=1,
+		Landscape=2,
+		Auto = Portrait | Landscape
+	}
+
+	public static class OrientationSerialize
+	{
+		public static string fromOrientation (Orientation v)
+		{
+			switch (v) {
+				case Orientation.Portrait: return "portrait";
+				case Orientation.Landscape: return "landscape";
+				case Orientation.Auto: return "portrait landscape";
+			}
+			throw new ArgumentOutOfRangeException ("v");
+		}
+
+		public static Orientation toOrientation (string v)
+		{
+			var hasPortrait = v.Contains ("portrait");
+			var hasLandscape = v.Contains ("landscape");
+
+			if (hasPortrait && hasLandscape) return Orientation.Auto;
+			if (hasPortrait) return Orientation.Portrait;
+			if (hasLandscape) return Orientation.Landscape;
+				
+			throw new ArgumentOutOfRangeException ("v");
+		}
+	}
+
+	public enum DeviceFamily
+	{
+		iPhone=1,
+		iPad=2,
+		Universal = iPhone | iPad
+	}
+
+	public static class  FamilySerialize
+	{
+		public static string fromFamily (DeviceFamily v)
+		{
+			switch (v) {
+				case DeviceFamily.iPhone: return "iphone";
+				case DeviceFamily.iPad: return "ipad";
+				case DeviceFamily.Universal: return "iphone ipad";
+			}
+			throw new ArgumentOutOfRangeException ("v");
+		}
+
+		public static DeviceFamily toFamily (string v)
+		{
+			var hasPhone = v.Contains ("iphone");
+			var hasPad = v.Contains ("ipad");
+
+			if (hasPhone && hasPad) return DeviceFamily.Universal;
+			if (hasPhone) return DeviceFamily.iPhone;
+			if (hasPad) return DeviceFamily.iPad;
+				
+			throw new ArgumentOutOfRangeException ("v");
+		}
+	}
+}
---
+++ src/Launchpad/Config/UISettings.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Launchpad.Forms;
+using UpdaterCore;
+
+namespace Launchpad
+{
+	public class ComboSetting<T> : Setting
+	{
+		public ComboSetting (ComboBox control,
+			Func<T, string> fromValue, Func<string, T> toValue)
+			: base (SettingType.Combo, control)
+		{
+			this.fromValue = fromValue;
+			this.toValue = toValue;
+
+			control.DataSource = Enum.GetValues (typeof(T));
+			control.MaxDropDownItems = control.Items.Count;
+			control.SelectedValueChanged += (s, ev) => onChanged();
+		}
+
+		public override string Value
+		{
+			get {
+				var value = (T)control.SelectedValue;
+				return fromValue (value);
+			}
+			set {
+				control.SelectedItem = toValue (value);
+			}
+		}
+
+		private ComboBox control {get { return (ComboBox)Control; }}
+		private readonly Func<T, string> fromValue;
+		private readonly Func<string, T> toValue;
+	}
+
+	public class BoolSetting : Setting
+	{
+		public BoolSetting (CheckBox control)
+			: base (SettingType.Boolean, control)
+		{
+			control.CheckedChanged += (s, e) => onChanged();
+		}
+
+		public override string Value
+		{
+			get { return ((CheckBox)Control).Checked.ToString(); }
+			set {
+				var result = value.Contains ("yes");
+				((CheckBox)Control).Checked = result;
+			}
+		}
+	}
+
+	public class FileSetting : Setting
+	{
+		public FileSetting (FileSelector control)
+			: base (SettingType.File, control)
+		{
+			control.inPath.TextChanged += (s, e) => onChanged();
+		}
+
+		public override string Value
+		{
+			get { return ((FileSelector)Control).Path; }
+			set { ((FileSelector)Control).Path = value; }
+		}
+	}
+
+	public class TextSetting : Setting
+	{
+		public TextSetting (TextBox text)
+			: base (SettingType.Text, text)
+		{
+			text.TextChanged += (s, e) => onChanged();
+		}
+
+		public override string Value
+		{
+			get { return ((TextBox)Control).Text; }
+			set { ((TextBox)Control).Text = value; }
+		}
+	}
+
+	public class Setting
+	{
+		public Setting (SettingType type, Object control)
+		{
+			this.Type = type;
+			this.Control = control;
+		}
+
+		public virtual string Value { get; set; }
+
+		public SettingType Type;
+		public Object Control;
+		public Action Changed;
+
+		protected void onChanged()
+		{
+			if (Changed != null)
+				Changed();
+		}
+	}
+
+	public enum SettingType
+	{
+		Text,
+		Boolean,
+		File,
+		Combo
+	}
+}
